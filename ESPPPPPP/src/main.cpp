cbc82732543ab47679ba5f2af4ccbf90615e0f29
mahmud209said <mahmud209said@gmail.com>
2026-02-14 14:57:56 +0200

    first commit

---
+++ ESPPPPPP/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+
+#define IR_PIN 4   // Change to the GPIO you connected the OUT pin to
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(IR_PIN, INPUT);
+
+    Serial.println("IR Sensor Test Started");
+}
+
+void loop() {
+    int sensorState = digitalRead(IR_PIN);
+
+    if (sensorState == LOW) {
+        Serial.println("                    Object detected");
+    } else {
+        Serial.println("                    No object detected");
+    }
+
+    delay(100);
+
+    
+}
