f07c80c39d7442409ef53ad4f7b54b8686da91b9
mahmud209said <mahmud209said@gmail.com>
2026-02-16 00:27:38 +0200

    m

--- ESP32 project/src/main.cpp
+++ ESP32 project/src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 #include <BasicLinearAlgebra.h>
 
+using namespace BLA;
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  BLA::Matrix<3, 3> A;
+  Serial.begin(115200);
+
+
+  BLA::Matrix<3> v;
+  v.Fill(0);
+
+    Serial.println(A);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+
 }
